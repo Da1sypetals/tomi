b531709b90e4bc5292d68d1d79b6b4a1eadf8b2d
Da1sypetals <da1sypetals.iota@gmail.com>
2025-05-29 20:18:25 +0800

    add ctor

--- snap-rs/src/cli_ops/memsnap.rs
+++ snap-rs/src/cli_ops/memsnap.rs
@@ -5,11 +5,30 @@ pub type AllocationIndex = usize;
 
 /// Options are lazily created
 pub struct MemSnap {
-    trace: Vec<Allocation>,
+    allocations: Vec<Allocation>,
 
-    timestamps: BTreeSet<usize>, // all timestamps that something happens
+    timestamps: BTreeSet<u32>, // all timestamps that something happens
 
     global_topk: Option<Vec<AllocationIndex>>,
 
     timestamp_topk: Option<BTreeMap<u32, AllocationIndex>>, // timestamp -> index
+
+                                                            // database: SqLite database
+}
+
+impl MemSnap {
+    pub fn new(allocations: Vec<Allocation>) -> Self {
+        let mut timestamps: BTreeSet<u32> = BTreeSet::new();
+
+        for alloc in &allocations {
+            timestamps.extend(alloc.timesteps.iter());
+        }
+
+        MemSnap {
+            allocations,
+            timestamps: timestamps,
+            global_topk: None,
+            timestamp_topk: None,
+        }
+    }
 }
