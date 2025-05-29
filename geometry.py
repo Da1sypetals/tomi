001a1998d6021e471350a18a2a4b540542e4ac10
Da1sypetals <da1sypetals.iota@gmail.com>
2025-05-29 17:37:05 +0800

    add geom

--- geometry.py
+++ geometry.py
@@ -2,14 +2,43 @@ from pydantic.dataclasses import dataclass
 import json
 
 
+@dataclass()
+class Frame:
+    name: str  # function name
+    filename: str
+    line: int
+
+
 @dataclass
 class Allocation:
     timesteps: list[int]
-    y: list[int]
+    offsets: list[int]
     size: int
+    callstack: list[Frame]
 
 
-def load_allocations(path: str):
-    with open(path) as f:
-        data = json.load(f)
-    return Allocation(data[])
+def load_allocations(alloc_path: str, elements_path: str):
+    with open(alloc_path) as f:
+        allocations = json.load(f)
+
+    with open(elements_path) as f:
+        elements = json.load(f)
+
+    callstacks = [[Frame(**frame) for frame in elem["frames"]] for elem in elements]
+
+    return [
+        Allocation(
+            timesteps=alloc["timesteps"],
+            offsets=alloc["offsets"],
+            size=alloc["size"],
+            callstack=callstack,
+        )
+        for alloc, callstack in zip(allocations, callstacks)
+    ]
+
+
+if __name__ == "__main__":
+    alloc_path = "snapshots/allocations.json"
+    elements_path = "snapshots/elements.json"
+    allocations = load_allocations(alloc_path, elements_path)
+    print(allocations)
